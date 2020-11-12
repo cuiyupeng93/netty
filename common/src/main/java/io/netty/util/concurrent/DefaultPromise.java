6aad2fe7e06abd4b92591b51d1f2de2c047a49d4
cuiyupeng@kanzhun.com <cuiyupeng@kanzhun.com>
2020-11-13 01:01:52 +0800

    netty源码学习的注释

--- common/src/main/java/io/netty/util/concurrent/DefaultPromise.java
+++ common/src/main/java/io/netty/util/concurrent/DefaultPromise.java
@@ -36,48 +36,68 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(DefaultPromise.class);
     private static final InternalLogger rejectedExecutionLogger =
             InternalLoggerFactory.getInstance(DefaultPromise.class.getName() + ".rejectedExecution");
+    // 最多可执行listeners的数量，默认是8
     private static final int MAX_LISTENER_STACK_DEPTH = Math.min(8,
             SystemPropertyUtil.getInt("io.netty.defaultPromise.maxListenerStackDepth", 8));
+    // 是一个原子更新器，通过CAS操作，原子化更新 DefaultPromise对象的名为result的成员，这个result成员是其异步操作判断的关键所在
     @SuppressWarnings("rawtypes")
     private static final AtomicReferenceFieldUpdater<DefaultPromise, Object> RESULT_UPDATER =
             AtomicReferenceFieldUpdater.newUpdater(DefaultPromise.class, Object.class, "result");
+    // 表示异步操作正常完成
     private static final Object SUCCESS = new Object();
+    // 表示异步操作不可取消，并且尚未完成
     private static final Object UNCANCELLABLE = new Object();
+    // CauseHolder，可以译为：原因持有者，顾名思义就是用来保存异常信息的，里面只保存着一个Throwable对象。
+    // 既然是保存了异常信息，那么它有值就表示操作异常结束了。可能是操作失败，或者操作取消(取消算是一种特殊的失败)，具体还要根据异常类型进一步判断。
     private static final CauseHolder CANCELLATION_CAUSE_HOLDER = new CauseHolder(ThrowableUtil.unknownStackTrace(
             new CancellationException(), DefaultPromise.class, "cancel(...)"));
     private static final StackTraceElement[] CANCELLATION_STACK = CANCELLATION_CAUSE_HOLDER.cause.getStackTrace();
 
+    // result 是判断这个异步操作是否完成的关键，result的取值有：SUCCESS 、UNCANCELLABLE、CauseHolder以及null
+    // 操作未完成：result == null || result == UNCANCELLABLE
+    // 操作完成：result != null && result != UNCANCELLABLE，表示异步操作完成，可能是操作成功，也可能是失败，具体结果还要结合cause一起判断
+    // 具体的判断条件如下：
+    // 操作成功：result != null && result != UNCANCELLABLE && !(result instanceof CauseHolder);
+    // 操作取消：result instanceof CauseHolder && ((CauseHolder) result).cause instanceof CancellationException;
+    // 操作失败：result instanceof CauseHolder && ((CauseHolder) result).cause 不是 CancellationException类型，是其他的异常类型，就是操作失败
+    //      （从这也可以看出取消操作也可以看作是操作失败的一种，只是取消操作的异常信息是特定的CancellationException）
     private volatile Object result;
+    // 就是Channel绑定的NioEventLoop
     private final EventExecutor executor;
     /**
-     * One or more listeners. Can be a {@link GenericFutureListener} or a {@link DefaultFutureListeners}.
-     * If {@code null}, it means either 1) no listeners were added yet or 2) all listeners were notified.
+     * 这个对象表示这个promise绑定的监听器
+     * 这个对象可以是两种类型：GenericFutureListener，或者 DefaultFutureListeners。
+     * 前者表示只有一个监听器，后者表示有多个监听器
+     * DefaultFutureListeners 其实是对GenericFutureListener的封装，它内部保存了一个GenericFutureListener的数组，表示多个监听器。
+     * 如果是null，可能是尚未添加监听器，或者已通知所有监听器
      *
-     * Threading - synchronized(this). We must support adding listeners when there is no EventExecutor.
+     * todo qa 2020-11-12 下面这句话没理解
+     * 线程-同步（this）。我们必须支持在没有EventExecutor时添加监听器。
      */
     private Object listeners;
     /**
-     * Threading - synchronized(this). We are required to hold the monitor to use Java's underlying wait()/notifyAll().
+     * 记录同步等待中的线程数量
+     * 比如serverBootstrap.sync方法，导致启动线程调用wait方法，此时waiters就会+1；
+     * 而我们调用setSuccess、trySuccess、setFailure、tryFailure、setUncancellable、cancel方法时，
+     * 将操作未完成改为操作完成，不论是哪种完成，都会唤醒这些等待的线程。
+     * todo qa 2020-11-12 下面这句话没理解
+     * 线程-同步（this）。我们需要保持监视器以使用Java的底层wait（）/notifyAll（）。
      */
     private short waiters;
 
     /**
-     * Threading - synchronized(this). We must prevent concurrent notification and FIFO listener notification if the
-     * executor changes.
+     * 是否需要唤醒的标志
+     * todo qa 2020-11-12 下面这句话没理解
+     * 线程-同步（this）。如果执行器发生变化，我们必须防止并发通知和FIFO侦听器通知。
      */
     private boolean notifyingListeners;
 
     /**
-     * Creates a new instance.
+     * 构造方法
+     * 通常使用 EventExecutor#newPromise() 方法来创建一个promise。这个方法会将自己作为EventExecutor参数传入
      *
-     * It is preferable to use {@link EventExecutor#newPromise()} to create a new promise
-     *
-     * @param executor
-     *        the {@link EventExecutor} which is used to notify the promise once it is complete.
-     *        It is assumed this executor will protect against {@link StackOverflowError} exceptions.
-     *        The executor may be used to avoid {@link StackOverflowError} by executing a {@link Runnable} if the stack
-     *        depth exceeds a threshold.
-     *
+     * @param executor 是一个事件执行器，更具体点，就是NioEventLoop。它用来在操作完成时通知这个promise。
+     *                 如果栈深超过阈值，executor还可以通过执行一个任务来避免StackOverflowError
      */
     public DefaultPromise(EventExecutor executor) {
         this.executor = checkNotNull(executor, "executor");
@@ -91,6 +111,9 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
         executor = null;
     }
 
+    // 设置操作成功，并且会通知监听器
+    // 由于通常是从 DefaultChannelPromise 调用上来的，所以这里的范型是Void
+    // 在DefaultChannelPromise中所有的set和try操作参数都是null，所以这里可以不考虑result为范型的情况
     @Override
     public Promise<V> setSuccess(V result) {
         if (setSuccess0(result)) {
@@ -119,6 +142,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 
     @Override
     public boolean setUncancellable() {
+        // 如果result对象还是null，可以直接使用CAS改为UNCANCELLABLE（也就是不可取消，也属于操作未完成状态）
         if (RESULT_UPDATER.compareAndSet(this, null, UNCANCELLABLE)) {
             return true;
         }
@@ -179,6 +203,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
             addListener0(listener);
         }
 
+        // 添加的时候会检查一次是不是已经完成了，如果已经完成了通知所有监听器
         if (isDone()) {
             notifyListeners();
         }
@@ -235,22 +260,30 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 
     @Override
     public Promise<V> await() throws InterruptedException {
+        // 已经完成直接返回
         if (isDone()) {
             return this;
         }
 
+        // 中断抛异常
         if (Thread.interrupted()) {
             throw new InterruptedException(toString());
         }
 
+        // 死锁抛异常
         checkDeadLock();
 
+        // 然后线程开始自旋等待异步操作完成
         synchronized (this) {
             while (!isDone()) {
+                // 每循环一次，就将waiters增加一
                 incWaiters();
                 try {
+                    // 线程wait，那么什么时候唤醒呢？
+                    // 在setSuccess、setFailure、cancel等方法中，将操作未完成改为操作完成后，都会通知所有wait的线程。
                     wait();
                 } finally {
+                    // 被唤醒后将waiters减一
                     decWaiters();
                 }
             }
@@ -375,10 +408,11 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
     /**
      * {@inheritDoc}
      *
-     * @param mayInterruptIfRunning this value has no effect in this implementation.
+     * @param mayInterruptIfRunning 这里的mayInterruptIfRunning没有任何用处，忽略它
      */
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
+        // 和setSuccess、setFailure一样，还是通过CAS修改result的状态，修改成功后通知监听器回调
         if (RESULT_UPDATER.compareAndSet(this, null, CANCELLATION_CAUSE_HOLDER)) {
             if (checkNotifyWaiters()) {
                 notifyListeners();
@@ -400,7 +434,9 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 
     @Override
     public Promise<V> sync() throws InterruptedException {
+        // 首先线程开始等待
         await();
+        // 等待异步操作完成后，再检查一下有没有异常
         rethrowIfFailed();
         return this;
     }
@@ -479,6 +515,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
         notifyListenerWithStackOverFlowProtection(eventExecutor, future, listener);
     }
 
+    // 通知监听器
     private void notifyListeners() {
         EventExecutor executor = executor();
         if (executor.inEventLoop()) {
@@ -487,6 +524,8 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
             if (stackDepth < MAX_LISTENER_STACK_DEPTH) {
                 threadLocals.setFutureListenerStackDepth(stackDepth + 1);
                 try {
+                    // 如果stackDepth<MAX_LISTENER_STACK_DEPTH，可以直接执行notifyListenersNow
+                    // 否则需要safeExecute，就是将notifyListenersNow任务提交到reactor线程的任务队列中
                     notifyListenersNow();
                 } finally {
                     threadLocals.setFutureListenerStackDepth(stackDepth);
@@ -545,7 +584,9 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
             this.listeners = null;
         }
         for (;;) {
+            // listeners 可能是 DefaultFutureListeners 或者 GenericFutureListener 类型的，所以要判断一下
             if (listeners instanceof DefaultFutureListeners) {
+                // 这个方法其实就是遍历一下数组，然后还是调用的 notifyListener0
                 notifyListeners0((DefaultFutureListeners) listeners);
             } else {
                 notifyListener0(this, (GenericFutureListener<?>) listeners);
@@ -574,6 +615,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private static void notifyListener0(Future future, GenericFutureListener l) {
         try {
+            // 从这里就看到了对监听器operationComplete方法的回调
             l.operationComplete(future);
         } catch (Throwable t) {
             if (logger.isWarnEnabled()) {
@@ -584,10 +626,13 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
 
     private void addListener0(GenericFutureListener<? extends Future<? super V>> listener) {
         if (listeners == null) {
+            // 添加一个监听器，listeners就是GenericFutureListener类型的
             listeners = listener;
         } else if (listeners instanceof DefaultFutureListeners) {
+            // 添加多个监听器，listeners就是DefaultFutureListeners类型的
             ((DefaultFutureListeners) listeners).add(listener);
         } else {
+            // 这种情况，说明listeners中已经有一个监听器了，这时候又添加一个，需要将它的类型改成DefaultFutureListeners
             listeners = new DefaultFutureListeners((GenericFutureListener<?>) listeners, listener);
         }
     }
@@ -609,9 +654,14 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
     }
 
     private boolean setValue0(Object objResult) {
+        // 通过CAS操作将result状态改为指定的一个对象，setSuccess和setFailure方法都会调这个方法
+        // 所以这里objResult可能是 SUCCESS，也可能是 cause，也可能是用户指定的其他对象，反正只要不是cause，都表示操作成功
+        // 前提得是操作未完成时，才可以标记状态
+        // 由于 result == null || result == UNCANCELLABLE 都表示操作未完成，所以这里会尝试两次CAS
         if (RESULT_UPDATER.compareAndSet(this, null, objResult) ||
             RESULT_UPDATER.compareAndSet(this, UNCANCELLABLE, objResult)) {
             if (checkNotifyWaiters()) {
+                // 通知监听器
                 notifyListeners();
             }
             return true;
@@ -620,8 +670,8 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
     }
 
     /**
-     * Check if there are any waiters and if so notify these.
-     * @return {@code true} if there are any listeners attached to the promise, {@code false} otherwise.
+     * 如果有waiter线程，全部唤醒
+     * 调用await的线程，会将waiters++
      */
     private synchronized boolean checkNotifyWaiters() {
         if (waiters > 0) {
